7fc0d3e44fa5781f0182ae6f04b5483040a97080
jandaluz <jamison.andaluz@gmail.com>
2018-07-06 11:44:25 -0400

    move conditional outside of return block

--- src/App.js
+++ src/App.js
@@ -52,30 +52,32 @@ class App extends Component {
       padding: '8px'
     };
 
+    let persons = null;
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age }
+            click={this.switchNameHandler.bind(this, "Jamison")}
+            changed={this.nameChangedHandler} >My Hobbies: Racing </Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
+        </div> 
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a react app!</h1>
         <p>This is really working!</p>
         <button
           style={style} 
-          onClick={this.togglePersonsHandler}>Switch Name</button>
-        { this.state.showPersons 
-          ?
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age} />
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age }
-                click={this.switchNameHandler.bind(this, "Jamison")}
-                changed={this.nameChangedHandler} >My Hobbies: Racing </Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age} />
-            </div> 
-          : null
-        }
+          onClick={this.togglePersonsHandler}>Show People</button>
+        {persons}
       </div>
     );
   }
